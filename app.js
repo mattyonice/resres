bc9594f3b378d3019c0c84290702c7799edf2b3d
mattyonice <mattyonice@gmail.com>
2022-05-10 17:05:23 +0530

    working toggle

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const toggleBtn = document.querySelector('.fs-1');
+const iconRev = document.getElementById('iconRev');
+
 
 toggleBtn.addEventListener('click', e => {
     console.log('btn clicked');
     var e = document.body;
     e.classList.toggle("dark");
 
-    // toggleBtn.classList.remove('text-dark');
-    // toggleBtn.classList.add('text-light');
-
     if (toggleBtn.classList.contains('text-dark')) {
         toggleBtn.classList.remove('text-dark');
         toggleBtn.classList.add('text-light');
+        iconRev.classList.remove('flip');
     }
     else {
         toggleBtn.classList.remove('text-light');
         toggleBtn.classList.add('text-dark');
-
+        iconRev.classList.add('flip');
     }
 
+    
 });
 
 // const toggleBtn = document.querySelector('.btn-dark');
